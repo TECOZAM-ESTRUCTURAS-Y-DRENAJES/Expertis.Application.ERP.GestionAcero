0e867557b848c6b9c6a80aac1ff3a27a0006127d
DavidVelascoHerrero1 <david.velasco@tecozam.com>
2022-05-26 10:19:36 +0200

    Ventana de acero totales
    Version 1.0
    Filtro por obra y fecha y estado.
    El dt se crea por código sin vista asociada al grid.

---
+++ Expertis.Application.ERP.GestionAcero/CIMedicionAceroTotales.Designer.vb
@@ -0,0 +1,181 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class CIMedicionAceroTotales
+    Inherits Solmicro.Expertis.Engine.UI.CIMnto
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim Grid_DesignTimeLayout As Janus.Windows.GridEX.GridEXLayout = New Janus.Windows.GridEX.GridEXLayout
+        Dim cmbEstado_DesignTimeLayout As Janus.Windows.GridEX.GridEXLayout = New Janus.Windows.GridEX.GridEXLayout
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(CIMedicionAceroTotales))
+        Me.cmbEstado = New Solmicro.Expertis.Engine.UI.ComboBox
+        Me.lblEstado = New Solmicro.Expertis.Engine.UI.Label
+        Me.clbFecha1 = New Solmicro.Expertis.Engine.UI.CalendarBox
+        Me.lblFecha1 = New Solmicro.Expertis.Engine.UI.Label
+        Me.clbFecha = New Solmicro.Expertis.Engine.UI.CalendarBox
+        Me.lblFecha = New Solmicro.Expertis.Engine.UI.Label
+        Me.advNObra = New Solmicro.Expertis.Engine.UI.AdvSearch
+        Me.lblNObra = New Solmicro.Expertis.Engine.UI.Label
+        Me.FilterPanel.SuspendLayout()
+        Me.CIMntoGridPanel.suspendlayout()
+        CType(Me.Grid, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.UiCommandManager1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Toolbar, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Menubar, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MainPanel.SuspendLayout()
+        Me.MainPanelCIMntoContainer.SuspendLayout()
+        CType(Me.cmbEstado, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'FilterPanel
+        '
+        Me.FilterPanel.Controls.Add(Me.cmbEstado)
+        Me.FilterPanel.Controls.Add(Me.lblEstado)
+        Me.FilterPanel.Controls.Add(Me.clbFecha1)
+        Me.FilterPanel.Controls.Add(Me.lblFecha1)
+        Me.FilterPanel.Controls.Add(Me.clbFecha)
+        Me.FilterPanel.Controls.Add(Me.lblFecha)
+        Me.FilterPanel.Controls.Add(Me.advNObra)
+        Me.FilterPanel.Controls.Add(Me.lblNObra)
+        Me.FilterPanel.Location = New System.Drawing.Point(0, 172)
+        Me.FilterPanel.Size = New System.Drawing.Size(536, 97)
+        '
+        'CIMntoGridPanel
+        '
+        Me.CIMntoGridPanel.Size = New System.Drawing.Size(536, 172)
+        '
+        'Grid
+        '
+        Grid_DesignTimeLayout.LayoutString = resources.GetString("Grid_DesignTimeLayout.LayoutString")
+        Me.Grid.DesignTimeLayout = Grid_DesignTimeLayout
+        Me.Grid.Font = New System.Drawing.Font("Verdana", 8.25!)
+        Me.Grid.Size = New System.Drawing.Size(536, 172)
+        '
+        'Toolbar
+        '
+        Me.Toolbar.Size = New System.Drawing.Size(245, 28)
+        '
+        'Menubar
+        '
+        Me.Menubar.Location = New System.Drawing.Point(0, 28)
+        '
+        'cmbEstado
+        '
+        cmbEstado_DesignTimeLayout.LayoutString = resources.GetString("cmbEstado_DesignTimeLayout.LayoutString")
+        Me.cmbEstado.DesignTimeLayout = cmbEstado_DesignTimeLayout
+        Me.cmbEstado.DisabledBackColor = System.Drawing.Color.White
+        Me.cmbEstado.Location = New System.Drawing.Point(105, 56)
+        Me.cmbEstado.Name = "cmbEstado"
+        Me.cmbEstado.SelectedIndex = -1
+        Me.cmbEstado.SelectedItem = Nothing
+        Me.cmbEstado.Size = New System.Drawing.Size(121, 21)
+        Me.cmbEstado.TabIndex = 22
+        Me.cmbEstado.Text = "Comenzado"
+        '
+        'lblEstado
+        '
+        Me.lblEstado.Location = New System.Drawing.Point(24, 57)
+        Me.lblEstado.Name = "lblEstado"
+        Me.lblEstado.Size = New System.Drawing.Size(45, 13)
+        Me.lblEstado.TabIndex = 23
+        Me.lblEstado.Text = "Estado"
+        '
+        'clbFecha1
+        '
+        Me.clbFecha1.DisabledBackColor = System.Drawing.Color.White
+        Me.clbFecha1.Location = New System.Drawing.Point(391, 49)
+        Me.clbFecha1.Name = "clbFecha1"
+        Me.clbFecha1.Size = New System.Drawing.Size(121, 21)
+        Me.clbFecha1.TabIndex = 20
+        '
+        'lblFecha1
+        '
+        Me.lblFecha1.Location = New System.Drawing.Point(327, 56)
+        Me.lblFecha1.Name = "lblFecha1"
+        Me.lblFecha1.Size = New System.Drawing.Size(62, 13)
+        Me.lblFecha1.TabIndex = 21
+        Me.lblFecha1.Text = "Fecha <="
+        '
+        'clbFecha
+        '
+        Me.clbFecha.DisabledBackColor = System.Drawing.Color.White
+        Me.clbFecha.Location = New System.Drawing.Point(391, 21)
+        Me.clbFecha.Name = "clbFecha"
+        Me.clbFecha.Size = New System.Drawing.Size(121, 21)
+        Me.clbFecha.TabIndex = 18
+        '
+        'lblFecha
+        '
+        Me.lblFecha.Location = New System.Drawing.Point(327, 24)
+        Me.lblFecha.Name = "lblFecha"
+        Me.lblFecha.Size = New System.Drawing.Size(62, 13)
+        Me.lblFecha.TabIndex = 19
+        Me.lblFecha.Text = "Fecha >="
+        '
+        'advNObra
+        '
+        Me.advNObra.DisabledBackColor = System.Drawing.Color.White
+        Me.advNObra.DisplayField = "NObra"
+        Me.advNObra.EntityName = "ObraCabecera"
+        Me.advNObra.Location = New System.Drawing.Point(105, 19)
+        Me.advNObra.Name = "advNObra"
+        Me.advNObra.PrimaryDataFields = "IDObra"
+        Me.advNObra.SecondaryDataFields = "IDObra"
+        Me.advNObra.Size = New System.Drawing.Size(121, 23)
+        Me.advNObra.TabIndex = 16
+        Me.advNObra.ViewName = "tbObraCabecera"
+        '
+        'lblNObra
+        '
+        Me.lblNObra.Location = New System.Drawing.Point(24, 27)
+        Me.lblNObra.Name = "lblNObra"
+        Me.lblNObra.Size = New System.Drawing.Size(43, 13)
+        Me.lblNObra.TabIndex = 17
+        Me.lblNObra.Text = "NObra"
+        '
+        'CIMedicionAceroTotales
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(544, 357)
+        Me.Name = "CIMedicionAceroTotales"
+        Me.Text = "CIMedicionAceroTotales"
+        Me.FilterPanel.ResumeLayout(False)
+        Me.FilterPanel.PerformLayout()
+        Me.CIMntoGridPanel.ResumeLayout(False)
+        CType(Me.Grid, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.UiCommandManager1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Toolbar, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Menubar, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MainPanel.ResumeLayout(False)
+        Me.MainPanelCIMntoContainer.ResumeLayout(False)
+        CType(Me.cmbEstado, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents cmbEstado As Solmicro.Expertis.Engine.UI.ComboBox
+    Friend WithEvents lblEstado As Solmicro.Expertis.Engine.UI.Label
+    Friend WithEvents clbFecha1 As Solmicro.Expertis.Engine.UI.CalendarBox
+    Friend WithEvents lblFecha1 As Solmicro.Expertis.Engine.UI.Label
+    Friend WithEvents clbFecha As Solmicro.Expertis.Engine.UI.CalendarBox
+    Friend WithEvents lblFecha As Solmicro.Expertis.Engine.UI.Label
+    Friend WithEvents advNObra As Solmicro.Expertis.Engine.UI.AdvSearch
+    Friend WithEvents lblNObra As Solmicro.Expertis.Engine.UI.Label
+End Class
---
+++ Expertis.Application.ERP.GestionAcero/CIMedicionAceroTotales.vb
@@ -0,0 +1,417 @@
+﻿Imports Solmicro.Expertis.Engine
+Imports Solmicro.Expertis.Engine.UI
+
+Public Class CIMedicionAceroTotales
+    Inherits Solmicro.Expertis.Engine.UI.CIMnto
+    Public Sub New()
+        MyBase.New()
+        'El Diseñador de Windows Forms requiere esta llamada.
+        InitializeComponent()
+        'Agregar cualquier inicialización después de la llamada a InitializeComponent()
+    End Sub
+    Private Sub CIMedicionAceroTotales_QueryExecuting(ByVal sender As System.Object, ByRef e As Solmicro.Expertis.Engine.UI.QueryExecutingEventArgs) Handles MyBase.QueryExecuting
+        'e.Filter.Add("NObra", FilterOperator.Equal, advNObra.Text, FilterType.String)
+        'If (cmbEstado.Text = "Proyectado") Then
+        '    e.Filter.Add("Estado", FilterOperator.Equal, 0, FilterType.Numeric)
+        'ElseIf (cmbEstado.Text = "Comenzado") Then
+        '    e.Filter.Add("Estado", FilterOperator.Equal, 1, FilterType.Numeric)
+        'ElseIf (cmbEstado.Text = "Terminado") Then
+        '    e.Filter.Add("Estado", FilterOperator.Equal, 2, FilterType.Numeric)
+        'End If
+        'e.Filter.Add("Fecha", FilterOperator.GreaterThanOrEqual, clbFecha.Value, FilterType.DateTime)
+        'e.Filter.Add("Fecha", FilterOperator.LessThanOrEqual, clbFecha1.Value, FilterType.DateTime)
+    End Sub
+
+    Private Sub CIMedicionAceroTotales_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        cmbEstado.DataSource = New EnumData("enumocEstado")
+    End Sub
+    Public Function estructuraTabla() As DataTable
+        Dim dt As New DataTable
+        Dim dc As New DataColumn("Obra")
+        dt.Columns.Add(dc)
+        dc = New DataColumn("Fecha")
+        dt.Columns.Add(dc)
+        dc = New DataColumn("D8", System.Type.GetType("System.Double"))
+        dt.Columns.Add(dc)
+        dc = New DataColumn("ED8", System.Type.GetType("System.Double"))
+        dt.Columns.Add(dc)
+        dc = New DataColumn("D10", System.Type.GetType("System.Double"))
+        dt.Columns.Add(dc)
+        dc = New DataColumn("ED10", System.Type.GetType("System.Double"))
+        dt.Columns.Add(dc)
+        dc = New DataColumn("D12", System.Type.GetType("System.Double"))
+        dt.Columns.Add(dc)
+        dc = New DataColumn("ED12", System.Type.GetType("System.Double"))
+        dt.Columns.Add(dc)
+        dc = New DataColumn("D16", System.Type.GetType("System.Double"))
+        dt.Columns.Add(dc)
+        dc = New DataColumn("ED16", System.Type.GetType("System.Double"))
+        dt.Columns.Add(dc)
+        dc = New DataColumn("D20", System.Type.GetType("System.Double"))
+        dt.Columns.Add(dc)
+        dc = New DataColumn("ED20", System.Type.GetType("System.Double"))
+        dt.Columns.Add(dc)
+        dc = New DataColumn("D25", System.Type.GetType("System.Double"))
+        dt.Columns.Add(dc)
+        dc = New DataColumn("ED25", System.Type.GetType("System.Double"))
+        dt.Columns.Add(dc)
+        dc = New DataColumn("D32", System.Type.GetType("System.Double"))
+        dt.Columns.Add(dc)
+        dc = New DataColumn("ED32", System.Type.GetType("System.Double"))
+        dt.Columns.Add(dc)
+        dc = New DataColumn("Transporte", System.Type.GetType("System.Double"))
+        dt.Columns.Add(dc)
+        dc = New DataColumn("ETransporte", System.Type.GetType("System.Double"))
+        dt.Columns.Add(dc)
+        dc = New DataColumn("Alambre", System.Type.GetType("System.Double"))
+        dt.Columns.Add(dc)
+        dc = New DataColumn("EAlambre", System.Type.GetType("System.Double"))
+        dt.Columns.Add(dc)
+        dc = New DataColumn("Mallazo", System.Type.GetType("System.Double"))
+        dt.Columns.Add(dc)
+        dc = New DataColumn("EMallazo", System.Type.GetType("System.Double"))
+        dt.Columns.Add(dc)
+        dc = New DataColumn("Porte", System.Type.GetType("System.Double"))
+        dt.Columns.Add(dc)
+        dc = New DataColumn("EPorte", System.Type.GetType("System.Double"))
+        dt.Columns.Add(dc)
+        dc = New DataColumn("Celosia", System.Type.GetType("System.Double"))
+        dt.Columns.Add(dc)
+        dc = New DataColumn("ECelosia", System.Type.GetType("System.Double"))
+        dt.Columns.Add(dc)
+        dc = New DataColumn("Estado")
+        dt.Columns.Add(dc)
+        Return dt
+    End Function
+
+    Public Sub rellenoTabla(ByVal dt As DataTable, ByVal obra As String, ByVal fechadesde As Date, ByVal fechahasta As Date, ByVal estado As String)
+        'Dos opciones: 
+        '- Para una obra especifica 
+        '- Para todas las obras
+
+        'Si es 0 = Para todas las obras
+        If obra.ToString.Length = 0 Then
+            Dim dtError As New DataTable
+            dtError = estructuraTabla()
+            Try
+                Dim D8 As Double = 0.0
+                Dim ED8 As Double = 0.0
+                Dim D10 As Double = 0.0
+                Dim ED10 As Double = 0.0
+                Dim D12 As Double = 0.0
+                Dim ED12 As Double = 0.0
+                Dim D16 As Double = 0.0
+                Dim ED16 As Double = 0.0
+                Dim D20 As Double = 0.0
+                Dim ED20 As Double = 0.0
+                Dim D25 As Double = 0.0
+                Dim ED25 As Double = 0.0
+                Dim D32 As Double = 0.0
+                Dim ED32 As Double = 0.0
+                Dim Transporte As Double = 0.0
+                Dim ETransporte As Double = 0.0
+                Dim Alambre As Double = 0.0
+                Dim EAlambre As Double = 0.0
+                Dim Mallazo As Double = 0.0
+                Dim EMallazo As Double = 0.0
+                Dim Porte As Double = 0.0
+                Dim EPorte As Double = 0.0
+                Dim Celosia As Double = 0.0
+                Dim ECelosia As Double = 0.0
+
+                Dim dtObra As New DataTable
+                Dim filtro As New Filter
+
+                Dim DE As New BE.DataEngine
+                filtro.Add("Fecha", FilterOperator.GreaterThanOrEqual, fechadesde)
+                filtro.Add("Fecha", FilterOperator.LessThanOrEqual, fechahasta)
+                If estado.ToString.Length <> 0 Then
+                    filtro.Add("Estado", FilterOperator.Equal, estado)
+                Else
+
+                End If
+
+                'En esta tabla vienen todos los datos correspondiente a este NObra
+                dtObra = DE.Filter("vMedicionAceroEstado", filtro, , "NObra")
+                'Creo la fila que insertaré en la tabla
+                Dim contador As Integer = 0
+                'Recorro dtObra y si la fila siguiente es distinta a la que estoy(Por NObra) me haces el total y agrego la fila
+                For Each fila As DataRow In dtObra.Rows
+                    Dim dr As DataRow
+                    D8 += dtObra.Rows(contador)("D8")
+                    D10 += dtObra.Rows(contador)("D10")
+                    D12 += dtObra.Rows(contador)("D12")
+                    D16 += dtObra.Rows(contador)("D16")
+                    D20 += dtObra.Rows(contador)("D20")
+                    D25 += dtObra.Rows(contador)("D25")
+                    D32 += dtObra.Rows(contador)("D32")
+                    ED8 += dtObra.Rows(contador)("ED8")
+                    ED10 += dtObra.Rows(contador)("ED10")
+                    ED12 += dtObra.Rows(contador)("ED12")
+                    ED16 += dtObra.Rows(contador)("ED16")
+                    ED20 += dtObra.Rows(contador)("ED20")
+                    ED25 += dtObra.Rows(contador)("ED25")
+                    ED32 += dtObra.Rows(contador)("ED32")
+                    '----
+                    Transporte += Nz(dtObra.Rows(contador)("Transporte"), 0)
+                    ETransporte += Nz(dtObra.Rows(contador)("ETransporte"), 0)
+                    Alambre += Nz(dtObra.Rows(contador)("Alambre"), 0)
+                    EAlambre += Nz(dtObra.Rows(contador)("EAlambre"), 0)
+                    Mallazo += Nz(dtObra.Rows(contador)("Mallazo"), 0)
+                    EMallazo += Nz(dtObra.Rows(contador)("EMallazo"), 0)
+                    Porte += Nz(dtObra.Rows(contador)("Porte"), 0)
+                    EPorte += Nz(dtObra.Rows(contador)("EPorte"), 0)
+                    Celosia += Nz(dtObra.Rows(contador)("Celosia"), 0)
+                    ECelosia += Nz(dtObra.Rows(contador)("ECelosia"), 0)
+
+                    'MsgBox(dtObra.Rows(contador)("NObra") & " - " & dtObra.Rows(contador + 1)("NObra"))
+                    Try
+                        If (dtObra.Rows(contador)("NObra") <> dtObra.Rows(contador + 1)("NObra")) Then
+                            dr = dt.NewRow
+                            dr("Obra") = dtObra.Rows(contador)("NObra")
+                            dr("Fecha") = fechadesde & " - " & fechahasta
+                            dr("D8") = D8
+                            dr("ED8") = ED8
+                            dr("D10") = D10
+                            dr("ED10") = ED10
+                            dr("D12") = D12
+                            dr("ED12") = ED12
+                            dr("D16") = D16
+                            dr("ED16") = ED16
+                            dr("D20") = D20
+                            dr("ED20") = ED20
+                            dr("D25") = D25
+                            dr("ED25") = ED25
+                            dr("D32") = D32
+                            dr("ED32") = ED32
+                            dr("Transporte") = Transporte
+                            dr("ETransporte") = ETransporte
+                            dr("Alambre") = Alambre
+                            dr("EAlambre") = EAlambre
+                            dr("Mallazo") = Mallazo
+                            dr("EMallazo") = EMallazo
+                            dr("Porte") = Porte
+                            dr("EPorte") = EPorte
+                            dr("Celosia") = Celosia
+                            dr("ECelosia") = ECelosia
+                            dr("Estado") = dtObra.Rows(contador)("Estado")
+                            dt.Rows.Add(dr)
+                            D8 = 0.0
+                            ED8 = 0.0
+                            D10 = 0.0
+                            ED10 = 0.0
+                            D12 = 0.0
+                            ED12 = 0.0
+                            D16 = 0.0
+                            ED16 = 0.0
+                            D20 = 0.0
+                            ED20 = 0.0
+                            D25 = 0.0
+                            ED25 = 0.0
+                            D32 = 0.0
+                            ED32 = 0.0
+                            Transporte = 0.0
+                            ETransporte = 0.0
+                            Alambre = 0.0
+                            EAlambre = 0.0
+                            Mallazo = 0.0
+                            EMallazo = 0.0
+                            Porte = 0.0
+                            EPorte = 0.0
+                            Celosia = 0.0
+                            ECelosia = 0.0
+                        Else
+
+                        End If
+                    Catch ex As Exception
+                        dr = dt.NewRow
+                        dr("Obra") = dtObra.Rows(contador)("NObra")
+                        dr("Fecha") = fechadesde & " - " & fechahasta
+                        dr("D8") = D8
+                        dr("ED8") = ED8
+                        dr("D10") = D10
+                        dr("ED10") = ED10
+                        dr("D12") = D12
+                        dr("ED12") = ED12
+                        dr("D16") = D16
+                        dr("ED16") = ED16
+                        dr("D20") = D20
+                        dr("ED20") = ED20
+                        dr("D25") = D25
+                        dr("ED25") = ED25
+                        dr("D32") = D32
+                        dr("ED32") = ED32
+                        dr("Transporte") = Transporte
+                        dr("ETransporte") = ETransporte
+                        dr("Alambre") = Alambre
+                        dr("EAlambre") = EAlambre
+                        dr("Mallazo") = Mallazo
+                        dr("EMallazo") = EMallazo
+                        dr("Porte") = Porte
+                        dr("EPorte") = EPorte
+                        dr("Celosia") = Celosia
+                        dr("ECelosia") = ECelosia
+                        dr("Estado") = dtObra.Rows(0)("Estado")
+                        dt.Rows.Add(dr)
+                    End Try
+                    contador += 1
+                Next
+                filtro.Clear()
+                Grid.DataSource = dt
+                dt = Nothing
+            Catch ex As Exception
+                MsgBox(ex.ToString)
+                Grid.DataSource = dtError
+                MsgBox("No existe ningún registro con estas características")
+            End Try
+        Else
+            'Creo todas las variables
+            Dim dtError As New DataTable
+            dtError = estructuraTabla()
+            Try
+                Dim D8 As Double = 0.0
+                Dim ED8 As Double = 0.0
+                Dim D10 As Double = 0.0
+                Dim ED10 As Double = 0.0
+                Dim D12 As Double = 0.0
+                Dim ED12 As Double = 0.0
+                Dim D16 As Double = 0.0
+                Dim ED16 As Double = 0.0
+                Dim D20 As Double = 0.0
+                Dim ED20 As Double = 0.0
+                Dim D25 As Double = 0.0
+                Dim ED25 As Double = 0.0
+                Dim D32 As Double = 0.0
+                Dim ED32 As Double = 0.0
+                Dim Transporte As Double = 0.0
+                Dim ETransporte As Double = 0.0
+                Dim Alambre As Double = 0.0
+                Dim EAlambre As Double = 0.0
+                Dim Mallazo As Double = 0.0
+                Dim EMallazo As Double = 0.0
+                Dim Porte As Double = 0.0
+                Dim EPorte As Double = 0.0
+                Dim Celosia As Double = 0.0
+                Dim ECelosia As Double = 0.0
+
+                Dim dtObra As New DataTable
+                Dim filtro As New Filter
+                Dim DE As New BE.DataEngine
+                filtro.Add("NObra", FilterOperator.Equal, obra)
+                filtro.Add("Fecha", FilterOperator.GreaterThanOrEqual, fechadesde)
+                filtro.Add("Fecha", FilterOperator.LessThanOrEqual, fechahasta)
+                If estado.ToString.Length <> 0 Then
+                    filtro.Add("Estado", FilterOperator.Equal, estado)
+                Else
+                End If
+                'En esta tabla vienen todos los datos correspondiente a este NObra
+                dtObra = DE.Filter("vMedicionAceroEstado", filtro)
+                'Creo la fila que insertaré en la tabla
+                Dim dr As DataRow
+                dr = dt.NewRow
+                'Le asigno el NObra
+                dr("Obra") = dtObra.Rows(0)("NObra")
+                'Le asgino la Fecha y saco la tabla con esas fechas
+                dr("Fecha") = fechadesde & " - " & fechahasta
+                'Obtengo todos los sumatorios de los campos
+                Dim contador As Integer = 0
+
+                For Each fila As DataRow In dtObra.Rows
+                    D8 += dtObra.Rows(contador)("D8")
+                    D10 += dtObra.Rows(contador)("D10")
+                    D12 += dtObra.Rows(contador)("D12")
+                    D16 += dtObra.Rows(contador)("D16")
+                    D20 += dtObra.Rows(contador)("D20")
+                    D25 += dtObra.Rows(contador)("D25")
+                    D32 += dtObra.Rows(contador)("D32")
+                    ED8 += dtObra.Rows(contador)("ED8")
+                    ED10 += dtObra.Rows(contador)("ED10")
+                    ED12 += dtObra.Rows(contador)("ED12")
+                    ED16 += dtObra.Rows(contador)("ED16")
+                    ED20 += dtObra.Rows(contador)("ED20")
+                    ED25 += dtObra.Rows(contador)("ED25")
+                    ED32 += dtObra.Rows(contador)("ED32")
+                    '----
+                    Transporte += Nz(dtObra.Rows(contador)("Transporte"), 0)
+                    ETransporte += Nz(dtObra.Rows(contador)("ETransporte"), 0)
+                    Alambre += Nz(dtObra.Rows(contador)("Alambre"), 0)
+                    EAlambre += Nz(dtObra.Rows(contador)("EAlambre"), 0)
+                    Mallazo += Nz(dtObra.Rows(contador)("Mallazo"), 0)
+                    EMallazo += Nz(dtObra.Rows(contador)("EMallazo"), 0)
+                    Porte += Nz(dtObra.Rows(contador)("Porte"), 0)
+                    EPorte += Nz(dtObra.Rows(contador)("EPorte"), 0)
+                    Celosia += Nz(dtObra.Rows(contador)("Celosia"), 0)
+                    ECelosia += Nz(dtObra.Rows(contador)("ECelosia"), 0)
+                    contador += 1
+                Next
+                dr("D8") = D8
+                dr("ED8") = ED8
+                dr("D10") = D10
+                dr("ED10") = ED10
+                dr("D12") = D12
+                dr("ED12") = ED12
+                dr("D16") = D16
+                dr("ED16") = ED16
+                dr("D20") = D20
+                dr("ED20") = ED20
+                dr("D25") = D25
+                dr("ED25") = ED25
+                dr("D32") = D32
+                dr("ED32") = ED32
+
+                dr("Transporte") = Transporte
+                dr("ETransporte") = ETransporte
+                dr("Alambre") = Alambre
+                dr("EAlambre") = EAlambre
+                dr("Mallazo") = Mallazo
+                dr("EMallazo") = EMallazo
+                dr("Porte") = Porte
+                dr("EPorte") = EPorte
+                dr("Celosia") = Celosia
+                dr("ECelosia") = ECelosia
+                dr("Estado") = dtObra.Rows(0)("Estado")
+                dt.Rows.Add(dr)
+                filtro.Clear()
+
+                Grid.DataSource = dt
+                dt = Nothing
+            Catch ex As Exception
+                Grid.DataSource = dtError
+                MsgBox("No existe ningún registro con estas características")
+            End Try
+
+        End If
+    End Sub
+
+    Public Sub creardt(ByVal obra As String, ByVal fechadesde As Date, ByVal fechahasta As Date, ByVal estado As String)
+        'Creacion Estructura Tabla
+        Dim dt As DataTable
+        dt = estructuraTabla()
+        'Relleno tabla
+        rellenoTabla(dt, obra, fechadesde, fechahasta, estado)
+
+    End Sub
+
+    Private Sub CIMedicionAceroTotales_QueryExecuted(ByVal sender As System.Object, ByRef e As Solmicro.Expertis.Engine.UI.QueryExecutedEventArgs) Handles MyBase.QueryExecuted
+        Dim obra As String
+        Dim fechadesde As Date
+        Dim fechahasta As Date
+        Dim estado As String
+
+        obra = Nz(advNObra.Text.ToString, "")
+        fechadesde = Nz(clbFecha.Value.ToString, "01/01/2000")
+        fechahasta = Nz(clbFecha1.Value.ToString, "31/12/2050")
+        If (cmbEstado.Text = "Proyectado") Then
+            estado = 0
+        ElseIf (cmbEstado.Text = "Comenzado") Then
+            estado = 1
+        ElseIf (cmbEstado.Text = "Terminado") Then
+            estado = 2
+        Else
+            estado = ""
+        End If
+        creardt(obra, fechadesde, fechahasta, estado)
+    End Sub
+
+
+End Class
